ab8a3fa75684116ea907f314aa2f5bcb8653ba3d
Daniela Lopez <leticia_daniela@live.com.mx>
2023-03-13 22:19:45 -0400

    responsive site implementation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/Lcsmall.png';
 import './navbar.css';
 
 //className="__navbar"
 //className="__navbar-links" are in BEM format (block element modifier)
+const Menu = () => (
+  <>
+  <p><a href="#aboutme">About Me</a></p>
+  <p><a href="#education">Education</a></p>
+  <p><a href="#experience">Experience</a></p>
+  <p><a href="#contact">Contact</a></p>
+  </>
+)
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="__navbar">
       <div className="__navbar-links"> 
@@ -13,13 +22,23 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="__navbar-links_container"> 
-        <p><a href="#aboutme">About Me</a></p>
-        <p><a href="#education">Education</a></p>
-        <p><a href="#experience">Experience</a></p>
-        <p><a href="#contact">Contact</a></p>
+      <Menu />
         </div>
       </div>
-      Navbar
+      <div className="__navbar-menu">
+        {toggleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="__navbar-menu_container scale-up-center">
+            <div className="__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
+      
+      </div>
     </div>
   )
 }
